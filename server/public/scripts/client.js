6f874fd2336842c9c496f853cf311127b2054963
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2018-09-26 16:24:53 -0500

    add config to MovieApp to connect html files to routes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,16 @@
 console.log('client connected!');
+
+const movieApp = angular.module('MovieApp', ['ngRoute']);
+
+movieApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '../views/home.html',
+        // controller: 'HomeController as vm'
+    }).when('/movies', {
+        templateUrl: '../views/movies.html',
+        // controller: 'MoviesController as vm'
+    }).when('/genres', {
+        templateUrl: '../views/genres.html',
+        // controller: 'GenresController as vm'
+    })
+}]); // end movieApp config
