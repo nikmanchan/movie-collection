a68e76a2d2ec249cb7fecaf54bc7b41745209425
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2018-10-01 09:04:10 -0500

    add insterts for movies

--- movies.sql
+++ movies.sql
@@ -22,6 +22,6 @@ VALUES
 INSERT INTO "movies"
 ("title", "genre_id", "release_date", "image_path", "run_time")
 VALUES
-('SCREAM', 3, '2001/10/20', 
-'http://cdn2us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/scream_2.jpg?itok=G6ZHZNOZ', 
-'1h 51m');
+('SCREAM', 7, '2001/10/20', 'http://cdn2us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/scream_2.jpg?itok=G6ZHZNOZ', '1h 51m'),
+('Despicable Me', 3, '2010/07/09', 'https://images-na.ssl-images-amazon.com/images/I/91FS9l0XJIL._SL1500_.jpg', '1h 34m'),
+('Star Wars: The Last Jedi', 1, '2017/12/15', 'https://upload.wikimedia.org/wikipedia/en/7/7f/Star_Wars_The_Last_Jedi.jpg', '2h 4m');
